5b5903cce46a4efdfe68b13c0fe512c1ce2876a0
apple <apple@ckj123.local>
2018-08-15 13:08:15 +0800

    加了点好东西

--- 2-4——2-11-CTF/baby step.py
+++
@@ -1,5 +0,0 @@
-from pyDes import *
-data="635353528311"
-k=des("DESCRYPT",CBC,"10000309",pad=None,padmode=PAD_PKCS5)
-d=k.encrypt(data)
-print("%s" %d)
--- 2-4——2-11-CTF/fangshemima.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# re=requests.get("http://p3xlhyup6.bkt.clouddn.com/Affine.py")
-# re.encoding='utf-8'
-# print(re.text)
-
-cipher = "1917090506070905195f07065f06031505195f035f0a07065f170c5f1407170205101105"
-
-print(cipher.encode(encoding='utf-8'))
